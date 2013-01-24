1a3dcbd19fcccfb876341bb1c6210ea731ca1081
Nando Vieira <fnando.vieira@gmail.com>
2013-01-24 14:56:48 -0200

    Damn you, no tests.

--- templates/troy.rb
+++ templates/troy.rb
@@ -5,7 +5,7 @@ Troy.configure do |config|
   # Make sure you download the correct file from
   # https://github.com/svenfuchs/rails-i18n/tree/master/rails/locale
   # and place it at `config/locales/*.yml`.
-  config.1i8n.load_path = Dir["config/locales/*.yml"]
+  config.i18n.load_path = Dir["config/locales/*.yml"]
 
   # These are the assets that you want
   # to precompile.
